73483223bc4d7a0c3e2f0b9e35528cb08a4d58e8
Marta Pacuszka <marta.pacuszka@cern.ch>
2019-07-16 01:09:20 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Container, Divider, Header, Icon, Button, Input } from 'semantic-ui-react';
+import {Map, Marker, Popup} from 'react-leaflet'
 
 function App() {
+  const position = [51.505, -0.09]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Container>
+    <Header as='h2' icon inverted textAlign='center'>
+          <Icon name='grid globe' />
+          Maptionary
+        </Header>
+    <Divider />
+    <div className="search-box">
+      <Input type='text' placeholder='Type word in English...' action>
+      <input />
+      <Button type='submit'>Search</Button>
+      </Input>
     </div>
+    <div className="map">
+      <Map center={position} zoom={13}>
+      <Marker position={position}>
+          <Popup>
+            A pretty CSS3 popup. <br/> Easily customizable.
+          </Popup>
+        </Marker>
+      </Map>
+    </div>
+    </Container>
   );
 }
 
